176b9f2bf34c98429fb03c44599f74e8f3bd4b7f
Jonathan Mauterer <jonathanmauterer@gmail.com>
2021-12-12 00:10:33 -0500

    test new directory push

--- header.php
+++ header.php
@@ -1,36 +1,37 @@
 <html>
 <head>
-<link rel="stylesheet" href="w3.css">
-<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="w3.css">
+  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
-<body>		
+
+<body>
 <header class="w3-container w3-padding" id="myHeader">
 <div class="w3-container w3-half" name="logo">
 <h3 class="w3-half" onclick="window.location='./admin.php'" style="position:relative; z-index:1;">CYCLEWRITE.COM</h3>
-    <p>This website is now a lesson in data recovery...</p>
+    <p>This website is now a lesson in data recovery.</p>
 </div>
-<div class="w3-container w3-half"> 
+<div class="w3-container w3-half">
 <div class="w3-container w3-right">
   <nav class="w3-topnav" style="position:relative; z-index:1;">
-   <a href="./">Home</a>
-<div class="w3-dropdown-hover">
-   <a href="browse.php">Browse</a>
-  <div class="w3-dropdown-content w3-border">
-    <a href="browse.php">Reading</a><br>
-    <a href="view.php?image=all">Pictures</a><br>
-    <a href="contributors.php">Contributors</a>
-  </div>
-</div>
-<div class="w3-dropdown-hover">
-   <a href="view.php?user="> </a>
-<div class="w3-dropdown-content w3-border">
-    <a href="home.php">Edit Profile</a>
-    <a href="login/logout.php">Logout</a>
-</div>  
-</div>
+       <a href="./">Home</a>
+    <div class="w3-dropdown-hover">
+       <a href="browse.php">Browse</a>
+      <div class="w3-dropdown-content w3-border">
+        <a href="browse.php">Reading</a>
+          <br>
+        <a href="view.php?image=all">Pictures</a>
+          <br>
+        <a href="contributors.php">Contributors</a>
+      </div>
+    </div>
+    <div class="w3-dropdown-hover">
+       <a href="view.php?user="> </a>
+    <div class="w3-dropdown-content w3-border">
+        <a href="home.php">Edit Profile</a>
+        <a href="login/logout.php">Logout</a>
+    </div>
+    </div>
   </nav>
 </div>
 </div>
 </header>
-
-
